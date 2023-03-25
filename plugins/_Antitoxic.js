4434da3b278adacc2762b706068a5fd66e9baf80
⍣ᴄʀɪᴡɪʟᴏᴘ࿐ <117367355+CRIWILOP@users.noreply.github.com>
2023-03-24 23:45:35 -0500

    🍀❥⍣ᴄʀɪᴡɪʟᴏᴘ࿐⛦🍀
    🍀❥⍣ᴄʀɪᴡɪʟᴏᴘ࿐⛦🍀

--- plugins/_Antitoxic.js
+++ plugins/_Antitoxic.js
@@ -1,4 +1,4 @@
-const toxicRegex = /puto|Puto|Puta|Vrga|Mrda|Mierda|mierda|pendejo|pendeja|Pendejo|Pendeja|verga|Verga| Pndj | pndj | pto | Pto | Pta | pta |hijueputa|Hijueputa|puta|rata| mrd |verga|vrga/i
+const toxicRegex = /puto|Puto|Puta|Vrga|Mrda|Mierda|mierda|pendejo|pendeja|Pendejo|Pendeja|verga|Verga| Pndj | pndj | pto | Pto | Pta | pta |hijueputa|Hijueputa|puta|rata| mrd |verga|vrga|hp|HP|Hp|UwU/i
 
 export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     if (m.isBaileys && m.fromMe)
@@ -12,12 +12,12 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     
     if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
        user.warn += 1
-       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
+       if (!(user.warn >= 1)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/☣️* advertencia`, false, { mentions: [m.sender] })
     }
     
-    if (user.warn >= 5) {
+    if (user.warn >= 1) {
        user.warn = 0
-       await m.reply(`Hola *@${m.sender.split`@`[0]}*, superaste las 5 advertencias serás bloqueado y eliminado de este grupo`, false, { mentions: [m.sender] })
+       await m.reply(`Hola *@${m.sender.split`@`[0]}*, *_no tolerante el comportamiento tóxico_*, *_serás bloqueado y eliminado de este grupo_*`, false, { mentions: [m.sender] })
        user.banned = true
        await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
        //await this.updateBlockStatus(m.sender, 'block')
